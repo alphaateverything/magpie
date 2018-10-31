085996002d3314df329da698f062baabf63a32d8
kakoatao <40969130+kakoatao@users.noreply.github.com>
2018-10-31 10:11:11 -0400

    Update Magpie2.java

--- Magpie/src/Magpie2.java
+++ Magpie/src/Magpie2.java
@@ -1,4 +1,23 @@
-
 public class Magpie2 {
-
+  //Starts the chatbot with a greeting
+  public String getGreeting() {
+    return "Hello, let's talk";
+  }
+  
+  //Responds to keywords
+  public String getResponse(String statement) {
+    String response = "";
+    
+    if (statement.indexOf("no") >= 0) {
+      response = "Why so negative?"
+    } else if (statement.indexOf("mother") >= 0 || statement.indexOf("father") >= 0 || statement.indexOf("brother") >= 0 || statement.indexOf("sister") >= 0) {
+      response = "Tell me more about your family";
+    } else {
+      response = getRandomResponse();
+    }
+    
+    return response;
+  }
+  
+  //
 }
